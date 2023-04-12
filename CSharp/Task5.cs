239278b97b345be52b787ea3c45dc411a71bf10a
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-13 02:08:38 +0300

    Create Task5.cs

---
+++ CSharp/Task5.cs
@@ -0,0 +1,14 @@
+public class TaskFive {
+    public static void Main(String[] args) {
+        string firstName = "Daemender";
+        string lastName = "Warlock";
+
+        Console.WriteLine(firstName, lastName);
+
+        var swapBuffer = firstName;
+        firstName = lastName;
+        lastName = swapBuffer;
+        //(firstName, lastName) = (lastName, firstName);
+        Console.WriteLine(firstName, lastName);
+    }
+}
