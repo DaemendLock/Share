3d8a86c05910d099e9ccd85a2a2b0646dfadbe52
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-13 01:22:36 +0300

    Task1

---
+++ CSharpJunior.cs
@@ -0,0 +1,18 @@
+//Not sure are Top-level statements allowed
+
+public class TaskOne {
+    public static void Main(String[] args) {
+        bool flag = false; //1 byte
+        byte byteVar = 0;
+        sbyte signedByteVar = 0;
+        char likeShortButNotShort = '\0';
+        short justTwoByteVar = 0;
+        ushort sameAsChar = 0;
+        int regularFourByteVar = 0;
+        uint sameRegularFourByteVarButWithoutSign = 0;
+        long eightByteVar = 0;
+        ulong eightByteVarButNoSign = 0;
+        string arrayOfChars = "";
+
+    }
+}
