92fa825849d31887b36cb6f3ac79bb62f022650c
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-13 20:11:19 +0300

    Update Task4.cs

--- CSharp/Task4.cs
+++ CSharp/Task4.cs
@@ -1,11 +1,11 @@
 public class TaskFour {
-    private const byte AMOUNT_OF_IMAGES_CONTAINED_IN_ROW = 3;
-    private const byte IMAGES_AMOUNT = 52;
+    private const byte ImagesContainedInRow = 3;
+    private const byte ImagesCount = 52;
 
     public static void Main(String[] args) {
-        byte rowAmmount = IMAGES_AMOUNT / AMOUNT_OF_IMAGES_CONTAINED_IN_ROW;
-        byte imagesLeft = IMAGES_AMOUNT % AMOUNT_OF_IMAGES_CONTAINED_IN_ROW;
+        byte filledRows = ImagesCount / ImagesContainedInRow;
+        byte imagesLeft = ImagesCount % ImagesContainedInRow;
         
-        Console.WriteLine($"Rows filled: {rowAmmount}{Environment.NewLine}Images left: {imagesLeft}");
+        Console.WriteLine($"Rows filled: {filledRows}{Environment.NewLine}Images left: {imagesLeft}");
     }
 }
