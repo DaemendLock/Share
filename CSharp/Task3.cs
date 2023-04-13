95100b92911742e1f5352ca8ad958f93e77fd3b3
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-13 20:08:50 +0300

    Update Task3.cs

--- CSharp/Task3.cs
+++ CSharp/Task3.cs
@@ -1,15 +1,14 @@
 public class TaskThree {
     public static void Main(String[] args) {
         Console.WriteLine("What is your name?");
-        string userName = Console.ReadLine();
+        string name = Console.ReadLine();
 
         Console.WriteLine("What is your zodiac?");
-        string userZodiac = Console.ReadLine();
+        string zodiac = Console.ReadLine();
 
-        //Cast is not required, check aswell
         Console.WriteLine("What is your age?");
-        string userAge = Console.ReadLine();
+        string age = Console.ReadLine();
 
-        Console.WriteLine($"Hello, {userName}, you appear to be {userZodiac}, {userAge} years old. Check your horoscope for more.");
+        Console.WriteLine($"Hello, {name}, you appear to be {zodiac}, {age} years old. Check your horoscope for more.");
     }
 }
