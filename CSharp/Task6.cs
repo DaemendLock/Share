ca3e1603123d00a866d63cab36dccd3f75190cf9
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 15:32:06 +0300

    Update Task6.cs

--- CSharp/Task6.cs
+++ CSharp/Task6.cs
@@ -6,7 +6,7 @@ public class TaskSix
         Console.WriteLine("Amount of gold:");
         uint userBalance = Convert.ToUInt43(Console.ReadLine());
 
-        Console.WriteLine($"Amount of crystals required(Crystals cost is {CRYSTAL_COST}):");
+        Console.WriteLine($"Crystals required(Crystals cost is {CRYSTAL_COST}):");
         uint crytalRequired = Convert.ToUInt32(Console.ReadLine());
 
         userBalance -= crytalAmountRequired * CrystalCost;
