c871fc0a260271215a9a0e719f1521fada11a62e
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 15:25:09 +0300

    Update Task4.cs

--- CSharp/Task4.cs
+++ CSharp/Task4.cs
@@ -1,8 +1,10 @@
-public class TaskFour {
+public class TaskFour
+{
     private const byte ImagesContainedInRow = 3;
     private const byte ImagesCount = 52;
 
-    public static void Main(String[] args) {
+    public static void Main(String[] args)
+    {
         byte filledRows = ImagesCount / ImagesContainedInRow;
         byte imagesLeft = ImagesCount % ImagesContainedInRow;
         
