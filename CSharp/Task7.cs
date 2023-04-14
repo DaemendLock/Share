bc4e228e356a605bf1557b50dd1d13c1a04d830c
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 19:51:34 +0300

    Update Task7.cs

--- CSharp/Task7.cs
+++ CSharp/Task7.cs
@@ -2,13 +2,13 @@ public class TaskSeven
 {
     public static void Main(String[] args)
     {
-        ulong WaitingTimeInMinutes = 10;
-        ulong MinutesContainedInHour = 60;
+        int WaitingTimeInMinutes = 10;
+        int MinutesContainedInHour = 60;
         
         Console.WriteLine("Queue length:");
-        uint queueLength = Convert.ToUInt32(Console.ReadLine());
-        ulong waitingTimeInMinutes = WAITING_TIME_IN_MINUTES * queueLength;
-        ulong waitingTimeInHours = waitingTimeInMinutes / MINUTES_CONTAINED_IN_HOUR;
+        int queueLength = Convert.ToInt32(Console.ReadLine());
+        int waitingTimeInMinutes = WAITING_TIME_IN_MINUTES * queueLength;
+        int waitingTimeInHours = waitingTimeInMinutes / MINUTES_CONTAINED_IN_HOUR;
 
         waitingTimeInMinutes = waitingTimeInMinutes - waitingTimeInHours * MINUTES_CONTAINED_IN_HOUR;
         Console.WriteLine($"Estimated time in line: {waitingTimeInHours}h {waitingTimeInMinutes}m.");
