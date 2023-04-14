6d24e102ac7a192610394b93d25774cde1da662c
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 15:38:18 +0300

    Update Task8.cs

--- CSharp/Task8.cs
+++ CSharp/Task8.cs
@@ -1,11 +1,13 @@
-public class TaskEight {
-    public static void Main(String[] args) {
+public class TaskEight
+{
+    public static void Main(String[] args)
+    {
         Console.WriteLine("Write message:");
         string message = Console.ReadLine() ?? "";
         Console.WriteLine("Set repeatitions count:");
-        uint repetitionsLeft = Convert.ToUInt32(Console.ReadLine());
-
-        for (; repetitionsLeft != 0; repetitionsLeft--) {
+        
+        for (uint repetitionsLeft = Convert.ToUInt32(Console.ReadLine()); repetitionsLeft != 0; repetitionsLeft--)
+        {
             Console.Write(message);
         }
     }
