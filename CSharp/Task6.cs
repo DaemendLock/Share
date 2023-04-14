63e26f11d4e8995ae0453887a6ec7fbf9a782230
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 15:29:30 +0300

    Update Task6.cs

--- CSharp/Task6.cs
+++ CSharp/Task6.cs
@@ -1,14 +1,15 @@
-public class TaskSix {
-    private const ulong CrystalCost = 10;
-
-    public static void Main(String[] args) {
+public class TaskSix
+{
+    public static void Main(String[] args)
+    {
+        uint CrystalCost = 10;
         Console.WriteLine("Amount of gold:");
-        ulong userBalance = Convert.ToUInt64(Console.ReadLine());
+        uint userBalance = Convert.ToUInt43(Console.ReadLine());
 
         Console.WriteLine($"Amount of crystals required(Crystals cost is {CRYSTAL_COST}):");
-        ulong crytalRequired = Convert.ToUInt64(Console.ReadLine());
+        uint crytalRequired = Convert.ToUInt32(Console.ReadLine());
 
-        userBalance -= crytalAmountRequired * CRYSTAL_COST;
+        userBalance -= crytalAmountRequired * CrystalCost;
         Console.WriteLine($"Gold: {userBalance}.{Environment.NewLine}Crystals: {crytalAmountRequired}.");
     }
 }
