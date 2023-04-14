096351b14a57e7e9a23234b8864103897c40fcc0
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 20:00:05 +0300

    Update Task13.cs

--- CSharp/Task13.cs
+++ CSharp/Task13.cs
@@ -1,23 +1,50 @@
-public class TaskThirteen {
-    private const string GuessCommand = "guess";
-    private const string SumCommand = "sum";
-    private const string RepeatCommand = "repeat";
-    private const string ExitCommand = "exit";
-
-    public static void Main(String[] args) {
+public class TaskThirteen
+{
+    public static void Main(String[] args)
+    {
+        const string GuessCommand = "guess";
+        const string SumCommand = "sum";
+        const string RepeatCommand = "repeat";
+        const string ExitCommand = "exit";
+        
         string lastCommand;
-        do {
+        
+        do
+        {
             Console.WriteLine("What is your command? sum? guess? repeat?");
             lastCommand = Console.ReadLine() ?? "";
+            
             switch (lastCommand) {
                 case GuessCommand:
-                    HandleGuessRequest();
+                    Console.WriteLine("Guess number from 0 to 100:");
+                    int guessedNumber = Convert.ToInt32(Console.ReadLine());
+                    
+                    while(guessedNumber > 100)
+                    {
+                        Console.WriteLine("Don't cheat. From 0 to 100.");
+                        guessedNumber = Convert.ToInt32(Console.ReadLine());
+                    }
+                    
+                    int randomNumber = new Random().Next(100);
+
+                    if(guessedNumber == randomNumber)
+                    {
+                        Console.WriteLine($"You guessed {randomNumber} right.");
+                    } else
+                    {
+                        Console.WriteLine($"You didn't guess {randomNumber} right.");
+                    }
+                    
                     break;
                 case SumCommand:
-                    HandleSumRequest();
+                    Console.WriteLine("Number to sum to:");
+                    int result = Convert.ToUInt32(Console.ReadLine());
+                    result = (result * result + result) >> 1;
+                    Console.WriteLine(result);
                     break;
                 case RepeatCommand:
-                    HandleRepeatRequest();
+                    Console.WriteLine("Text to repeat:");
+                    Console.WriteLine(Console.ReadLine() ?? "Null");
                     break;
                 case ExitCommand:
                     continue;
@@ -25,32 +52,5 @@ public class TaskThirteen {
                     throw new Exception("Unknown command " + lastCommand);
             };
         } while (lastCommand.Equals(ExitCommand) == false);
-    }
-    private static void HandleGuessRequest() {
-        Console.WriteLine("Guess number from 0 to 100:");
-        byte guessedNumber = Convert.ToByte(Console.ReadLine());
-        while(guessedNumber > 100) {
-            Console.WriteLine("Don't cheat. From 0 to 100.");
-            guessedNumber = Convert.ToByte(Console.ReadLine());
-        }
-        byte randomNumber = (byte) new Random().Next(100);
-
-        if(guessedNumber == randomNumber) {
-            Console.WriteLine($"You guessed {randomNumber} right.");
-        } else {
-            Console.WriteLine($"You didn't guess {randomNumber} right.");
-        }
-    }
-
-    private static void HandleSumRequest() {
-        Console.WriteLine("Number to sum to:");
-        uint result = Convert.ToUInt32(Console.ReadLine());
-        result = (result * result + result) >> 1;
-        Console.WriteLine(result);
-    }
-
-    private static void HandleRepeatRequest() {
-        Console.WriteLine("Text to repeat:");
-        Console.WriteLine(Console.ReadLine() ?? "Null");
     }
 }
