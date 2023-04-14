be51733ecf31ed8f7bc5868af1df0f595ef360db
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-14 23:14:16 +0300

    Create Task16.cs

---
+++ CSharp/Task16.cs
@@ -0,0 +1,21 @@
+public class TaskSixteen
+{
+    public static void Main(String[] args)
+    {
+        int n = new Random().Next(1, 27);
+        int count = 0;
+        int value = n * 4;
+
+        while (value <= 1000)
+        {
+            if(value >= 99)
+            {
+                count++;
+            }
+
+            value += n;
+        }
+
+        Console.WriteLine(count);
+    }
+}
