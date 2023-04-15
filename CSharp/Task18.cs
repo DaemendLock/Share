b039f2705195de17b85a8bbb15b9079b125b1df3
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-15 15:14:23 +0300

    Update Task18.cs

--- CSharp/Task18.cs
+++ CSharp/Task18.cs
@@ -4,23 +4,28 @@ public class TaskEightteen
     {
         string text = Console.ReadLine();
         int balance = 0;
+        int maxBalance = 0;
 
         foreach (char symbol in text)
         {
             if (symbol == '(')
             {
                 balance ++;
+                
+                if (maxBalance < balance)
+                    maxBalance = balance;
                 continue;
             }
 
             balance--;
 
             if(balance < 0)
-            {
                 break;
-            }
         }
 
         Console.WriteLine(balance == 0);
+        
+        if (balance == 0)
+            Console.WriteLine("Max depth: " + maxBalance);
     }
 }
