1d286dcd08912bb99c8146aaf8b96abdbc4ec495
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-15 13:42:32 +0300

    Update Task13.cs

--- CSharp/Task13.cs
+++ CSharp/Task13.cs
@@ -7,6 +7,8 @@ public class TaskThirteen
         const string RepeatCommand = "repeat";
         const string ExitCommand = "exit";
         
+        int maxGuessNumber = 100;
+        
         string lastCommand;
         
         do
@@ -16,16 +18,16 @@ public class TaskThirteen
             
             switch (lastCommand) {
                 case GuessCommand:
-                    Console.WriteLine("Guess number from 0 to 100:");
+                    Console.WriteLine($"Guess number from 0 to {maxGuessNumber}:");
                     int guessedNumber = Convert.ToInt32(Console.ReadLine());
                     
-                    while(guessedNumber > 100)
+                    while(guessedNumber > maxGuessNumber)
                     {
-                        Console.WriteLine("Don't cheat. From 0 to 100.");
+                        Console.WriteLine($"Don't cheat. From 0 to {maxGuessNumber}.");
                         guessedNumber = Convert.ToInt32(Console.ReadLine());
                     }
                     
-                    int randomNumber = new Random().Next(100);
+                    int randomNumber = new Random().Next(maxGuessNumber+1);
 
                     if(guessedNumber == randomNumber)
                     {
