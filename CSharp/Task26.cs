d109a7eaca37a8ded90212728abd1ad83585888c
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-16 23:41:38 +0300

    Create Task26.cs

---
+++ CSharp/Task26.cs
@@ -0,0 +1,10 @@
+public class TaskTwelveSix{
+    public static void Main(String[] args) {
+        string[] words = Console.ReadLine().Split();
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
