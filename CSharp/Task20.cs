6b277588ed9e71697d0ec0ef56c9be91c6d40490
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-16 20:27:25 +0300

    Update Task20.cs

--- CSharp/Task20.cs
+++ CSharp/Task20.cs
@@ -2,40 +2,40 @@ public class TaskTwelve
 {
     public static void Main(String[] args)
     { 
-        int sumInRow = 1;
-        int multiplyInColumn = 0;
+        int rowToGetSum = 1;
+        int columnToMultiply = 0;
         int randomMaxValue = 9;
 
-        int arrayRows = 5;
-        int arrayColumns = 2;
+        int matrixRowsCount = 5;
+        int matrixColumnsCount = 2;
 
-        int[, ] array = new int[arrayRows, arrayColumns];
+        int[, ] numbers = new int[matrixRowsCount, matrixColumnsCount];
 
         Random randomValuesSource = new Random();
 
-        for (int i = 0; i < arrayColumns; i++)
+        for (int i = 0; i < matrixRowsCount; i++)
         {
-            for (int j = 0; j < arrayColumns; j++)
+            for (int j = 0; j < matrixColumnsCount; j++)
             {
-                array[i, j] = randomValuesSource.Next(randomMaxValue + 1);
-                Console.Write(array[i, j] + " ");
+                numbers[i, j] = randomValuesSource.Next(randomMaxValue + 1);
+                Console.Write(numbers[i, j] + " ");
             }
 
-            Console.Write(Environment.NewLine);
+            Console.WriteLine();
         }
 
         int sumResult = 0;
 
-        for (int i = 0; i < arrayColumns; i++)
+        for (int i = 0; i < matrixColumnsCount; i++)
         {
-            sumResult += array[sumInRow, i];
+            sumResult += numbers[rowToGetSum, i];
         }
 
         long multiplicationResult = 1;
 
-        for (int i = 0; i < arrayRows; i++)
+        for (int i = 0; i < matrixRowsCount; i++)
         {
-            multiplicationResult *= array[i, multiplyInColumn];
+            multiplicationResult *= numbers[i, columnToMultiply];
         }
 
         Console.WriteLine($"Sum: {sumResult}, Multiplication: {multiplicationResult}");
