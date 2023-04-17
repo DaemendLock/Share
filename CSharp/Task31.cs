74ca8e6260ab0b9635867ca7e3531f3a5889b66a
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-04-17 20:22:19 +0300

    Update Task31.cs

--- CSharp/Task31.cs
+++ CSharp/Task31.cs
@@ -4,36 +4,43 @@ public class TaskThirtyOne
     {
         int arrayLength = 10;
         int randomNumberMaxValue = 9;
-        int[] numbers = new int [arrayLength];
+        int[] numbers = new int[arrayLength];
 
         Random random = new Random();
 
         for (int i = 0; i < numbers.Length; i++)
         {
             numbers[i] = random.Next(randomNumberMaxValue + 1);
-            Console.Write(numbers[i]);
         }
+
+        PrintArray(numbers);
 
         Shuffle(numbers, random);
-        Console.WriteLine();
 
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i]);
-        }
+        PrintArray(numbers);
+        
     }
 
     private static void Shuffle(int[] numbers, Random randomValuesSource)
     {
-        Shuffle(numbers, randomValuesSource, numbers.Length);
+        Shuffle(numbers, randomValuesSource, numbers.Length * 2);
     }
 
     private static void Shuffle(int[] numbers, Random randomValuesSource, int shuffleDepth)
     {
         for (int i = 0; i < shuffleDepth; i++)
         {
-            int swapWith = randomValuesSource.Next(numbers.Length);
+            int swapWith = randomValuesSource.Next(1, numbers.Length);
             (numbers[0], numbers[swapWith]) = (numbers[swapWith], numbers[0]);
         }
+    }
+
+    private static void PrintArray(int[] numbers)
+    {
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
     }
 }
