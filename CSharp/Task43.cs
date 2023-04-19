b76453242f4153175dae5d78f0cdded90ad173d6
Panchenko_Artem <daemendlordandclashmaster200@gmail.com>
2023-04-19 14:56:49 +0300

    Revert "Update Task43.cs"
    This reverts commit 7e76d93c238d9315a39899305190e981ff9e6566.

--- CSharp/Task43.cs
+++ CSharp/Task43.cs
@@ -1,153 +1,94 @@
-public class TaskTwentyEight
+public class TaskTrade
 {
     public static void Main(String[] args)
     {
-        const string AddCommand = "1";
-        const string PrintCommand = "2";
-        const string RemoveCommand = "3";
-        const string FindCommand = "4";
-        const string ExitCommand = "5";
+        Customer customer = new Customer();
+        Trader trader = new Trader();
+        trader.Give(new Item("Rusty sword", "Old rusted sword of your dad."));
 
-        string[] names = new string[0];
-        string[] positions = new string[0];
+        Console.WriteLine("Trader has:");
+        trader.ShowItems();
+        Console.WriteLine("Customer has:");
+        customer.ShowItems();
 
-        bool exitRequested = false;
-        string input;
+        Console.WriteLine("Trying get your father's sword");
+        customer.TryBuy("Rusty sword", trader);
 
-        while (exitRequested == false)
+        Console.WriteLine("Trader has:");
+        trader.ShowItems();
+        Console.WriteLine("Customer has:");
+        customer.ShowItems();
+    }
+}
+
+public class Item
+{
+    public Item(string title, string description)
+    {
+        Title = title;
+        Description = description;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return Title + " - " + Description;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Item && ((Item) obj).Title.Equals(Title);
+    }
+}
+
+public abstract class ItemHolder
+{
+    private LinkedList<Item> _items = new LinkedList<Item>();
+
+    public void ShowItems()
+    {
+        foreach (Item item in _items)
         {
-            Console.WriteLine($"Write command: add - {AddCommand}, print - {PrintCommand}, remove - {RemoveCommand}, find - {FindCommand}, exit - {ExitCommand}");
-            input = Console.ReadLine();
-
-            switch (input)
-            {
-                case AddCommand:
-                    AddFile(ref names, ref positions);
-                    break;
-
-                case PrintCommand:
-                    Print(names, positions);
-                    break;
-
-                case RemoveCommand:
-                    RemoveFile(ref names, ref positions);
-                    break;
-
-                case FindCommand:
-                    FindByName(names, positions);
-                    break;
-
-                case ExitCommand:
-                    exitRequested = true;
-                    break;
-
-                default:
-                    Console.Error.WriteLine("Failed to read input.");
-                    break;
-            }
+            Console.WriteLine(item);
         }
     }
 
-    private static void AddFile(ref string[] names, ref string[] positions)
+    public void Give(Item item)
     {
-        names = ExtendArray(names, "Write name: ");
-        positions = ExtendArray(positions, "Write position: ");
+        _items.AddLast(item);
     }
 
-    private static void RemoveFile(ref string[] names, ref string[] positions)
+    public Item TakeItem(string title)
     {
-        if (names.Length == 0)
-        {
-            Console.Error.WriteLine("0 files stored.");
-        }
+        Item result = Find(title);
 
-        Console.WriteLine("Write file index");
-        int index;
-
-        while (int.TryParse(Console.ReadLine(), out index) == false)
-        {
-            Console.Error.WriteLine("Can't parse index.");
-        }
-
-        index--;
-        names = RemoveAt(index, names);
-        positions = RemoveAt(index, positions);
-    }
-
-    private static void Print(string[] names, string[] positions)
-    {
-        if (names.Length== 0)
-        {
-            Console.Error.WriteLine("0 files printed.");
-        }
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            Console.WriteLine($"{i + 1}: {names[i]}  - {positions[i]}");
-        }
-    }
-
-    private static void FindByName(string[] names, string[] positions)
-    {
-        if (names.Length == 0)
-        {
-            Console.Error.WriteLine("0 files stored.");
-        }
-
-        Console.WriteLine("Write surname for lookup: ");
-        string surname = Console.ReadLine();
-
-        int foundIndex = -1;
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            if (names[i].Split()[0].Equals(surname))
-            {
-                Console.WriteLine($"{i + 1}: {names[i]} - {positions[i]}");
-                foundIndex = i;
-            }
-        }
-
-        if (foundIndex == -1)
-        {
-            Console.Error.WriteLine("Can't find this surname");
-        }
-    }
-
-    private static string[] ExtendArray(string[] array, string requestMessage)
-    {
-        string[] result = new string[array.Length + 1];
-
-        Console.WriteLine(requestMessage);
-        string value = Console.ReadLine();
-
-        for (int i = 0; i < array.Length; i++)
-        {
-            result[i] = array[i];
-        }
-
-        result[result.Length - 1] = value;
-
+        _items.Remove(result);
         return result;
     }
 
-    private static string[] RemoveAt(int index, string[] array)
+    public Item Find(string title)
     {
-        if (index >= array.Length || index < 0)
-        {
-            Console.Error.WriteLine("Can't remove by index " + (index + 1));
-            return array;
-        }
-
-        (array[array.Length - 1], array[index]) = (array[index], array[array.Length - 1]);
-
-        string[] result = new string[array.Length - 1];
-
-        for (int i = 0; i < result.Length; i++)
-        {
-            result[i] = array[i];
-        }
-
-        return result;
+        return _items.First((Item item) => item.Title.Equals(title));
     }
+}
+
+public class Customer : ItemHolder
+{
+    public bool TryBuy(string title, Trader trader)
+    {
+        Item item = trader.TakeItem(title);
+        if (item != null)
+        {
+            Give(item);
+        }
+
+        return item != null;
+    }
+}
+
+public class Trader : ItemHolder
+{
+
 }
