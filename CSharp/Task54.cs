65824503b1b47c3449efff0ef003acd92b9886c6
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-05-11 22:02:18 +0300

    Update Task54.cs

--- CSharp/Task54.cs
+++ CSharp/Task54.cs
@@ -1,9 +1,9 @@
-using static Utils;
-
 public class Task54
 {
     public static void Main(String[] args)
     {
+        int takeTop = 3;
+
         List<Player> players = new List<Player>()
         {
             new Player("tsumuri", 80, 41730),
@@ -19,9 +19,9 @@ public class Task54
         };
 
         Console.WriteLine("Top level:");
-        Console.WriteLine(string.Join(Environment.NewLine, players.OrderByDescending(player => player.Level).Take(3)));
+        Console.WriteLine(string.Join(Environment.NewLine, players.OrderByDescending(player => player.Level).Take(takeTop)));
         Console.WriteLine("Top score:");
-        Console.WriteLine(string.Join(Environment.NewLine, players.OrderByDescending(player => player.Score).Take(3)));
+        Console.WriteLine(string.Join(Environment.NewLine, players.OrderByDescending(player => player.Score).Take(takeTop)));
     }
 }
 
@@ -40,52 +40,5 @@ public class Player
     public override string ToString()
     {
         return $"{Nickname} - Lv.{Level}. (Score: {Score})";
-    }
-}
-
-public static class Utils
-{
-    public readonly static Random Random = new Random();
-
-    public static string ReadResponse(string message)
-    {
-        Console.WriteLine(message);
-
-        return Console.ReadLine();
-    }
-
-    public static int ForceReadInt(string message = "Write number.", int minValue = int.MinValue, int maxValue = int.MaxValue)
-    {
-        Console.WriteLine(message);
-
-        int result;
-
-        while (int.TryParse(Console.ReadLine(), out result) == false || result < minValue || result >= maxValue)
-        {
-            Console.Error.WriteLine("Failed to read. Try again.");
-        }
-
-        return result;
-    }
-
-    public static int? ReadChoose(string message, params string[] responses)
-    {
-        foreach (string resposne in responses)
-        {
-            Console.WriteLine("-" + resposne);
-        }
-
-        string choice = ReadResponse(message);
-
-        for (int i = 0; i < responses.Length; i++)
-        {
-            if (responses[i].Equals(choice, StringComparison.OrdinalIgnoreCase))
-            {
-                return i;
-            }
-        }
-
-        Console.Error.WriteLine("Failed to read choose.");
-        return null;
     }
 }
