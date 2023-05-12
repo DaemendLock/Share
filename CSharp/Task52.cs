bde4464e629dacbbf6038d611927100d31c7a796
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-05-12 18:56:20 +0300

    Update Task52.cs

--- CSharp/Task52.cs
+++ CSharp/Task52.cs
@@ -14,7 +14,7 @@ public class Task52
             Console.WriteLine(criminal);
         }
 
-        criminals = criminals.Where(criminal => criminal.Crime.StartsWith(AmnestiCrime)).ToList();
+        criminals = criminals.Where(criminal => criminal.Crime.StartsWith(AmnestiCrime) == false).ToList();
         Console.WriteLine("New criminals list:");
 
         foreach (Criminal criminal in criminals)
@@ -38,52 +38,5 @@ public class Criminal
     public override string ToString()
     {
         return $"Criminal: \"{Fullname}\", Crime: \"{Crime}\"";
-    }
-}
-
-public static class Utils
-{
-    public readonly static Random Random = new Random();
-
-    public static string ReadResponse(string message)
-    {
-        Console.WriteLine(message);
-
-        return Console.ReadLine();
-    }
-
-    public static int ForceReadInt(string message = "Write number.", int minValue = int.MinValue, int maxValue = int.MaxValue)
-    {
-        Console.WriteLine(message);
-
-        int result;
-
-        while (int.TryParse(Console.ReadLine(), out result) == false || result < minValue || result >= maxValue)
-        {
-            Console.Error.WriteLine("Failed to read. Try again.");
-        }
-
-        return result;
-    }
-
-    public static int? ReadChoose(string message, params string[] responses)
-    {
-        foreach (string resposne in responses)
-        {
-            Console.WriteLine("-" + resposne);
-        }
-
-        string choice = ReadResponse(message);
-
-        for (int i = 0; i < responses.Length; i++)
-        {
-            if (responses[i].Equals(choice, StringComparison.OrdinalIgnoreCase))
-            {
-                return i;
-            }
-        }
-
-        Console.Error.WriteLine("Failed to read choose.");
-        return null;
     }
 }
