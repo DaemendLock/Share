aa5f4902fe35a9c2aa933f6a0abe9d8526d9ffe7
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-06-25 21:35:01 +0300

    Add files via upload

--- Unity/Spawners/Assets/Scripts/SpawnPoint.cs
+++ Unity/Spawners/Assets/Scripts/SpawnPoint.cs
@@ -2,20 +2,4 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] private GameObject _unit;
-
-    private void Start()
-    {
-        SpawnController.RegisterSpawner(this);
-    }
-
-    public virtual void TriggerSpawn()
-    {
-        if (enabled == false)
-        {
-            return;
-        }
-        
-        Instantiate(_unit, transform.position, transform.rotation, transform.parent);
-    }
 }
---
+++ Unity/Spawners/Assets/Scripts/Spawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private float _spawnCooldown;
+    [SerializeField] private Enemy _spawnObject;
+    [SerializeField] private SpawnPoint[] _spawnLocations;
+    
+    private int _spawnIndex = 0;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnCycle());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(SpawnCycle());
+    }
+
+    private IEnumerator SpawnCycle()
+    {
+        while (enabled)
+        {
+            SpawnNext();
+            yield return new WaitForSeconds(_spawnCooldown);
+        }
+    }
+
+    private void SpawnNext()
+    {
+        SpawnPoint position = FindNextSpawnpoint();
+
+        if (position == null)
+        {
+            return;
+        }
+
+        Transform spawnPosition = position.transform;
+        Instantiate(_spawnObject, spawnPosition.position, spawnPosition.rotation, transform.parent);
+    }
+
+    private SpawnPoint FindNextSpawnpoint()
+    {
+        if (_spawnLocations.Length == 0)
+        {
+            return null;
+        }
+
+        for (int i = 1; i < _spawnLocations.Length; i++)
+        {
+            if (_spawnLocations[(_spawnIndex + i) % _spawnLocations.Length].enabled)
+            {
+                _spawnIndex = (_spawnIndex + i) % _spawnLocations.Length;
+                return _spawnLocations[_spawnIndex];
+            }
+        }
+
+        return null;
+    }
+}
