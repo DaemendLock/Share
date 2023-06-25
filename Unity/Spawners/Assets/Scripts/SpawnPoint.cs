b015903793661e8339fefc0a736eecc790d2e6a9
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-06-25 19:20:00 +0300

    Update SpawnPoint.cs

--- Unity/Spawners/Assets/Scripts/SpawnPoint.cs
+++ Unity/Spawners/Assets/Scripts/SpawnPoint.cs
@@ -11,7 +11,7 @@ public class SpawnPoint : MonoBehaviour
 
     public virtual void TriggerSpawn()
     {
-        if(enabled == false)
+        if (enabled == false)
         {
             return;
         }
