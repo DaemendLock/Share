1917a0d062bc728aa09619582242249f04593b6e
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-07-13 23:37:07 +0300

    Add files via upload

--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : Entity
 {
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpForce = 20;
+    [Min(0.0001f), SerializeField] private float _grondDistanceToJump = 0.01f;
+
     private Rigidbody2D _rigidbody;
 
     private void Start()
@@ -10,13 +14,14 @@ public class Player : Entity
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    public void Jump(float jumpForce)
+    public void Jump()
     {
-        _rigidbody.velocity += new Vector2(0, Mass * jumpForce);
+        _rigidbody.velocity += new Vector2(0, Mass * _jumpForce);
     }
 
-    public void Walk(float speed, Vector2 direction)
+    public void Walk(Vector2 direction)
     {
+        float speed = _moveSpeed * Mass;
         _rigidbody.velocity = new Vector2(speed * direction.x, _rigidbody.velocity.y * direction.y);
     }
 
@@ -25,11 +30,11 @@ public class Player : Entity
         _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
     }
 
-    public bool OnGround(float groundHigh)
+    public bool OnGround()
     {
         float sizeMultiplier = 0.5f;
         float highMultiplier = 2;
-        
-        return Physics2D.BoxCast(transform.position - new Vector3(0, transform.localScale.y * (sizeMultiplier + groundHigh * highMultiplier), 0) , new Vector2(transform.localScale.x, groundHigh), 0 , Vector2.down, transform.localScale.y * groundHigh);
+
+        return Physics2D.BoxCast(transform.position - new Vector3(0, transform.localScale.y * (sizeMultiplier + _grondDistanceToJump * highMultiplier), 0), new Vector2(transform.localScale.x, _grondDistanceToJump), 0, Vector2.down, transform.localScale.y * _grondDistanceToJump);
     }
 }
--- Platformer/Assets/Scripts/PlayerInputReader.cs
+++ Platformer/Assets/Scripts/PlayerInputReader.cs
@@ -2,18 +2,14 @@ using UnityEngine;
 
 [RequireComponent(typeof(Player))]
 public class PlayerInputReader : MonoBehaviour
-{
-    [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _jumpForce = 20;
-    [Min(0.0001f), SerializeField] private float _grondDistanceToJump = 0.01f;
-
-    private Player _player;
-
+{ 
     #region KeyBinds
     [Header("Keybinds"), SerializeField] private KeyCode _left = KeyCode.Q;
     [SerializeField] private KeyCode _right = KeyCode.E;
     [SerializeField] private KeyCode _jump = KeyCode.W;
     #endregion
+
+    private Player _player;
 
     private void Start()
     {
@@ -22,14 +18,14 @@ public class PlayerInputReader : MonoBehaviour
 
     private void Update()
     {
-        if (_player.OnGround(_grondDistanceToJump) == false)
+        if (_player.OnGround() == false)
         {
             return;
         }
 
         if (Input.GetKeyDown(_jump))
         {
-            _player.Jump(_jumpForce);
+            _player.Jump();
         }
 
         if (Input.GetKey(_left) == Input.GetKey(_right))
@@ -40,13 +36,13 @@ public class PlayerInputReader : MonoBehaviour
         
         if (Input.GetKey(_left))
         {
-            _player.Walk(_moveSpeed * _player.Mass, Vector2.left);
+            _player.Walk(Vector2.left);
             return;
         }
 
         if (Input.GetKey(_right))
         {
-            _player.Walk(_moveSpeed * _player.Mass, Vector2.right);
+            _player.Walk(Vector2.right);
         }
     }
 }
