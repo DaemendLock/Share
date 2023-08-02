a4174a1a6cb7e99fc7f8f46a076efd9ba3337dc1
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-08-02 15:58:51 +0300

    Update UnitHealth.cs

--- Unity/Healthbar/Scripts/UnitHealth.cs
+++ Unity/Healthbar/Scripts/UnitHealth.cs
@@ -11,6 +11,11 @@ public class UnitHealth : MonoBehaviour
     public float Health => _health;
     public float MaxHealth => _maxHealth;
 
+    private void Start()
+    {
+        _health = _maxHealth;
+    }
+
     public void ModifyHealth(float value)
     {
         _health = Mathf.Clamp(_health + value, 0, _maxHealth);
