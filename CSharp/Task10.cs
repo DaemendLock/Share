5a2776daf008b62b6a7b50749730993a6d184e69
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2024-07-20 16:54:31 +0300

    Update Task10.cs

--- CSharp/Task10.cs
+++ CSharp/Task10.cs
@@ -2,11 +2,11 @@ public class TaskTen
 {
     public static void Main(String[] args)
     {
-        int CycleIncrement = 7;
-        int FirstValue = 5;
-        int LastValue = 96;
-        
-        for (int i = FirstValue; i <= LastValue; i += CycleIncrement)
+        int cycleIncrement = 7;
+        int firstValue = 5;
+        int lastValue = 96;
+
+        for (int i = firstValue; i <= lastValue; i += cycleIncrement)
         {
             Console.Write($"{i} ");
         }
