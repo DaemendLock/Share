706abf86c3aad47b98cbbd8dcccc62b63b0c9d84
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2024-07-20 17:01:46 +0300

    Update Task11.cs

--- CSharp/Task11.cs
+++ CSharp/Task11.cs
@@ -5,24 +5,24 @@ public class TaskEleven
         int firstDivider = 3;
         int secondDivider = 5;
         int maximumValue = 100;
-        
+
         int generatedValue = new Random().Next(maximumValue + 1);
-        
+
         int addendumCount = generatedValue / firstDivider;
         int resultSum = (firstDivider * (1 + addendumCount) * addendumCount) / 2;
         addendumCount = generatedValue / secondDivider;
         resultSum += (secondDivider * (1 + addendumCount) * addendumCount) / 2;
-        
-        int lcm = firstDivider;
-        
-        while (lcm % secondDivider != 0)
+
+        int leastCommonMultiple = firstDivider;
+
+        while (leastCommonMultiple % secondDivider != 0)
         {
-            lcm += firstDivider;
+            leastCommonMultiple += firstDivider;
         }
-        
-        addendumCount = generatedValue / lcm;
-        resultSum -= (lcm * (1 + addendumCount) * addendumCount) / 2;
-        
-        Console.WriteLine(generatedValue + " "+resultSum);
+
+        addendumCount = generatedValue / leastCommonMultiple;
+        resultSum -= (leastCommonMultiple * (1 + addendumCount) * addendumCount) / 2;
+
+        Console.WriteLine(generatedValue + " " + resultSum);
     }
 }
